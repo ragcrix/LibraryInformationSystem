16a3a5acaf5d2d70aae80416ccf71581edc8dc60
ragcrix <yigitcannalci@gmail.com>
2020-01-15 22:28:03 +0300

    logging added

--- src/main/java/com/medium/libraryinfosystem/service/impl/BookServiceImpl.java
+++ src/main/java/com/medium/libraryinfosystem/service/impl/BookServiceImpl.java
@@ -3,6 +3,7 @@ package com.medium.libraryinfosystem.service.impl;
 import com.medium.libraryinfosystem.model.lombok.Book;
 import com.medium.libraryinfosystem.repository.BookRepository;
 import com.medium.libraryinfosystem.service.BookService;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -11,6 +12,7 @@ import java.util.List;
 /**
  * @author ragcrix
  */
+@Slf4j
 @Service
 public class BookServiceImpl implements BookService {
     @Autowired
@@ -18,21 +20,25 @@ public class BookServiceImpl implements BookService {
 
     @Override
     public Book findByName(String name) {
+        log.info("inside findByName()");
         return bookRepository.findByName(name);
     }
 
     @Override
     public List<Book> findAll() {
+        log.info("inside findAll()");
         return bookRepository.findAll();
     }
 
     @Override
     public Book save(Book book) {
+        log.info("inside save()");
         return bookRepository.save(book);
     }
 
     @Override
     public void delete(Book book) {
+        log.info("inside delete()");
         bookRepository.delete(book);
     }
 
